2ce8169d30addc1477750a1d20b08e3b3293a4bd
Andres Gumucio <andresgum@tamu.edu>
2025-08-09 16:13:12 -0500

    added Stepper library in project by adding code in platformio.ini file; tested and debugged stepper motor

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,27 @@
 #include <Arduino.h>
+#include <Stepper.h> // use built-in Stepper library for Arduino
 
-// put function declarations here:
+#define STEPS_PER_REVOLUTION 2048 // define a constant (2048 steps for full rev.)
+
+Stepper myStepper(STEPS_PER_REVOLUTION, 8, 10, 9, 11); // had to change 9,10 order
 
 void setup() {
-  // put your setup code here, to run once:
+  // setSpeed() sets motor speed in RPM
+  myStepper.setSpeed(15); // doesn't make motor turn, just sets speed at which it will when step() is called
   Serial.begin(9600);
-  Serial.println("Potentiometer Test Starting...");
 }
 
 void loop() {
-  int potValue = analogRead(A0);
-  Serial.print("Potentiometer Value: ");
-  Serial.println(potValue);
-  delay(200);
+    // rotate motor one full revolution clockwise
+  Serial.println("Moving clockwise...");
+  // step() turns motor a specific number of steps
+  myStepper.step(STEPS_PER_REVOLUTION);
+  delay(500);
+  
+  // rotate motor one full revolution counter-clockwise
+  Serial.println("Moving counter-clockwise...");
+  myStepper.step(-STEPS_PER_REVOLUTION);
+  delay(500);
 }
 
-// put function definitions here:
+
